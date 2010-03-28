cfe2e0580a51c4d08743c122133ed8f29ef4869f
sam <sam@localhost>
2010-03-28 14:34:30 -0400

    Finished fleshing out individual

--- individual.cpp
+++ individual.cpp
@@ -3,6 +3,7 @@
                       path in the TSP.
 */
 
+#include <algorithm>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
@@ -14,21 +15,31 @@ using namespace std;
 
    }
 
-   double Individual::Fitness() {
-      return this->fitness;
-   }
- 
-   double Individual::Raw_Fitness() {
-      return this->raw_fitness;
-   }
+   double Individual::Fitness() { return this->fitness; }
+   
+   double Individual::Raw_Fitness() { return this->raw_fitness; }
    
    /*
       Selects two cities at random and exchanges them.
    */
    void Individual::Mutate() {
-
+      int city1, city2;
+      // Generate two random indices in { 0, ..., length(chromosome) }
+      city1 = rand() % this->chromosome.size();
+      city2 = rand() % this->chromosome.size();
+      
+      // Ensure the cities are unique.
+      while(city1 == city2) { city2 = rand() % a.size(); }
+      
+      swap(this->chromosome[city1], this->chromosome[city2]);
    }
    
    void Individual::Print() {
-   
+      for(vector<int>::iterator iter = this->chromosome.begin();
+          iter != this->chromosome.end(); ++iter) {
+         cout << *iter << " ";
+      }
+      cout << endl;
+      cout << this->raw_fitness << endl;
+      cout << this->fitness << endl;
    }

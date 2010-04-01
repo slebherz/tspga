ea1f1c2975976b934bec3f4346a30beaa11e5623
apsillers <apsillers@localhost>
2010-04-01 14:59:37 -0400

    population.cpp now reads in cost file upon construction; added double_explode()

--- explode.h
+++ explode.h
@@ -1,5 +1,5 @@
 /*
-explode.h
+File: explode.h
 
 Based on Zedwood's freely available explode function at
 http://www.zedwood.com/article/106/cpp-explode-function
@@ -49,6 +49,44 @@ vector<int> int_explode( const string &delimiter, const string &str)
         }
     }
     arr.push_back(  atoi(str.substr(k, i-k).c_str()) );
+    return arr;
+}
+
+/*
+function: double_explode
+
+Similar to explode(), but assumes all elements are doubles and builds a vector
+of doubles.
+
+*/
+vector<double> double_explode( const string &delimiter, const string &str)
+{
+    vector<double> arr;
+
+    int strleng = str.length();
+    int delleng = delimiter.length();
+    if (delleng==0)
+        return arr;//no change
+
+    int i=0; 
+    int k=0;
+    while( i<strleng )
+    {
+        int j=0;
+        while (i+j<strleng && j<delleng && str[i+j]==delimiter[j])
+            j++;
+        if (j==delleng)//found delimiter
+        {
+            arr.push_back(  strtod(str.substr(k, i-k).c_str(), NULL ) );
+            i+=delleng;
+            k=i;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    arr.push_back(  strtod(str.substr(k, i-k).c_str(), NULL) );
     return arr;
 }
 #endif
